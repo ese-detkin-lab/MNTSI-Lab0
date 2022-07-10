3b4913d86858f79d87dab08c8cf615a9b5db96de
Andrew Katz <aakatz3@gmail.com>
2022-07-10 15:06:05 -0400

    Update Lab0.1_Blink.ino

--- Lab0.1_Blink/Lab0.1_Blink.ino
+++ Lab0.1_Blink/Lab0.1_Blink.ino
@@ -3,19 +3,19 @@
 
   Turns an LED on and off repeatedly.
 
-  The LED must be connected ot pin 13.
+  The LED must be connected ot pin 2.
 */
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin 2 as an output.
   pinMode(2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
-  delay(1500);            // wait for a second and a half
+  delay(1500);           // wait for a second and a half
   digitalWrite(2, LOW);  // turn the LED off by making the voltage LOW
-  delay(500);             // wait for half a second
+  delay(500);            // wait for half a second
 }
